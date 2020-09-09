90ab63699ed1f2dc413fee234f9cb48daade3280
qinyi <2671412803@qq.com>
2020-09-09 18:03:45 +0800

    输出额外css

--- inc/customized-plugin.php
+++ inc/customized-plugin.php
@@ -376,5 +376,14 @@ beian.png' data-src='' data-was-processed='true'> <a href='" . esc_url($police_b
 	echo "版权所有 © " . $date . $this_year . " <a href='" . esc_url($bolg_href) . "'>" . esc_html($blog_name) . "</a> " . $police_beian_exit . " | <a href='http://www.beian.miit.gov.cn/' rel='external nofollow' target='_blank'>" . $beian . "</a><br/>Theme Jasmine By <span id='rm-rf-run'>rm-rf-run</span> With  | All Rights Reserved<br/>" . $startDate;
 }
 
+function echo_extraCss(){
+  if (get_option('jasmine_extraCss')) {
+    echo '<style>'.get_option('jasmine_extraCss').'</style>';
+  }
+}
+
+//额外css
+add_action('wp_head','echo_extraCss')
+
 ?>
 
