fe3064a0e377baac16fac0dae71e1ba122e89b26
qinyi <2671412803@qq.com>
2021-06-18 16:47:50 +0800

    问题描述:取值不正确 问题单号:无 问题原因:取值不正确 解决方法:修改取值方法

--- template-parts/nav/nav-post.php
+++ template-parts/nav/nav-post.php
@@ -24,7 +24,7 @@
     当前位置：
     <li><a href="<?php bloginfo('url');?>" rel="bookmark">首页</a></li>
     <?php $category = get_the_category();if ($category) {?>
-    <li>&nbsp;>&nbsp;<a href="<?php echo esc_url(get_category_link($category->term_id)); ?>" rel="bookmark"><?php the_category(', ');}?></a></li>
+    <li>&nbsp;>&nbsp;<a href="<?php echo esc_url(get_category_link($category[0]->term_id)); ?>" rel="bookmark"><?php the_category(', ');}?></a></li>
     <?php if (is_singular()) {?>
     <li>&nbsp;>&nbsp;<a href="<?php esc_url(the_permalink());?>" rel="bookmark"><?php the_title();}?></a></li>
   </ol>
